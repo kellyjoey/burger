53ea966f527a58407244c5713929c91a2caa9de7
Kelly Nicholson <Kelly>
2017-04-05 21:44:34 -0400

    fixing port

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 8080;
-// var port = process.env.PORT;
+// var port = 8080;
+var port = process.env.PORT;
 
 var app = express();
 
